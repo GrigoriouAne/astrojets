548ef9dd34f321d9636323a88be84463860e7546
Stavros <stavros.karachristianidis@alphaomegazed.com>
2025-05-31 14:25:36 +0300

    navbar responsive

--- src/layout/layout.jsx
+++ src/layout/layout.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/navbar/navbar";
+import styles from "./layout.module.css";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className={styles.container}>
       <Navbar />
       <Outlet />
-    </>
+    </div>
   );
 }
