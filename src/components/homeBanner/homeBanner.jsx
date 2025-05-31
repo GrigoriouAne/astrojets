ba103e05763dfa6b03ea04bc6c87af12a50118e7
Stavros <stavros.karachristianidis@alphaomegazed.com>
2025-05-31 15:27:07 +0300

    home banner

--- src/components/homeBanner/homeBanner.jsx
+++ src/components/homeBanner/homeBanner.jsx
@@ -1,21 +1,34 @@
-import styles from "./homeBanner.module.css";
+import { useEffect } from "react";
+import styles from "./HomeBanner.module.css";
 
-export default function HomeBanner() {
+const HomeBanner = () => {
+  useEffect(() => {
+    // Fix iOS 100vh issue
+    const setHeight = () => {
+      document.documentElement.style.setProperty(
+        "--vh",
+        `${window.innerHeight * 0.01}px`
+      );
+    };
+    setHeight();
+    window.addEventListener("resize", setHeight);
+    return () => window.removeEventListener("resize", setHeight);
+  }, []);
+
   return (
-    <section className={styles.banner}>
+    <div className={styles.banner}>
       <div className={styles.videoWrapper}>
         <iframe
           className={styles.video}
           src="https://www.youtube-nocookie.com/embed/HSYBZ2MC9GU?autoplay=1&mute=1&controls=0&showinfo=0&modestbranding=1&rel=0&playsinline=1&loop=1&playlist=HSYBZ2MC9GU&enablejsapi=1"
-          frameBorder="0"
           allow="autoplay; fullscreen"
           allowFullScreen
           title="Background Video"
         />
       </div>
-      <div className={styles.overlay}>
-        <h1 className={styles.title}>Your Catchy Title</h1>
-      </div>
-    </section>
+      <h1 className={styles.title}>Your Title Here</h1>
+    </div>
   );
-}
+};
+
+export default HomeBanner;
