282d11c9a2771be472585cd305d824e32b3aa47e
Stavros <stavros.karachristianidis@alphaomegazed.com>
2025-07-01 10:56:32 +0300

    about us and info page background fixed

--- src/components/map/map.jsx
+++ src/components/map/map.jsx
@@ -3,7 +3,7 @@ import styles from "./map.module.css";
 const Map = () => {
   return (
     <iframe
-      src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3018.4264710533766!2d24.303536176418525!3d40.840559629734805!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDUwJzI2LjAiTiAyNMKwMTgnMjIuMCJF!5e0!3m2!1sen!2sgr!4v1750000559850!5m2!1sen!2sgr"
+      src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d2497.6700373099816!2d24.30523935748146!3d40.840540061502566!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNDDCsDUwJzI2LjAiTiAyNMKwMTgnMjIuMCJF!5e1!3m2!1sen!2sgr!4v1751355419829!5m2!1sen!2sgr"
       //   width="600"
       //   height="450"
       //   style="border:0;"
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -36,22 +36,18 @@ const Plans = () => {
 const AboutUs = () => {
   return (
     <div id="aboutUs" className={styles.aboutUsContainer}>
-      <h1 className={styles.aboutUsTitle}>About Us</h1>
-      <p className={styles.aboutUsText}>
-        👊 AstroJets – Two friends, one dream, endless rides We are the
-        AstroBoys. Two childhood friends who grew up on cartoons, endless
-        summers, and the feeling that life’s better when you have someone next
-        to you who balances you out. The first boy is the voice of reason —
-        responsible, grounded, always thinking ahead. The second boy is the
-        spark — a dreamer, spontaneous, full of "let's go now!" energy. They may
-        seem like opposites. But that’s exactly what makes it work. Where one
-        hits the brakes, the other pushes the throttle. And somewhere between
-        logic and imagination, AstroJets was born — inspired by childhood
-        cartoons, driven by a shared dream, and fueled by real friendship.
-        Today, in Nea Peramos, Kavala, we don’t just rent out jet skis. We offer
-        experiences. Memories. Stories you’ll tell at the end of the summer.
-        AstroJets When imagination meets responsibility… and becomes a wave. 🌊
-      </p>
+      <div className={styles.aboutUsTitleContainer}>
+        <h1 className={styles.aboutUsTitle}>About Us</h1>
+      </div>
+      <div className={styles.aboutUsTextContainer}>
+        <p className={styles.aboutUsText}>
+          We are the AstroBoys. Two friends with opposite minds but one shared
+          dream — to bring our childhood imagination to life. One boy thinks
+          with logic. The other with heart. Together, they balance — just like
+          on a jet ski. That’s how AstroJets was born. In Nea Peramos, Kavala,
+          we don’t just rent jet skis. We create memories.
+        </p>
+      </div>
     </div>
   );
 };
@@ -59,31 +55,38 @@ const AboutUs = () => {
 const Info = () => {
   return (
     <div id="info" className={styles.infoContainer}>
-      <h1 className={styles.infoTitle}>Contact Us</h1>
-      <div className={styles.infoCard}>
-        <Icon source={phoneIcon} />
-        <a className={styles.infoLink} href="tel:6980083496">
-          6980083496
-        </a>
-        <a className={styles.infoLink} href="tel:6984785608">
-          6984785608
-        </a>
-        <Icon source={emailIcon} />
-        <a className={styles.emailLink} href="mailto:astrojets.ws@gmail.com">
-          astrojets.ws@gmail.com
-        </a>
+      <div className={styles.infoTitleContainer}>
+        <h1 className={styles.infoTitle}>Contact Us</h1>
+      </div>
+      <div className={styles.infoContentContainer}>
+        <div className={styles.infoCard}>
+          <Icon source={phoneIcon} />
+          <a className={styles.infoLink} href="tel:6980083496">
+            +30 6980083496
+          </a>
+          <a className={styles.infoLink} href="tel:6984785608">
+            +30 6984785608
+          </a>
+          <Icon source={emailIcon} />
+          <a className={styles.emailLink} href="mailto:astrojets.ws@gmail.com">
+            astrojets.ws@gmail.com
+          </a>
 
-        <h1 className={styles.mapTitle}>Social media</h1>
-        <div className={styles.socialMediaContainer}>
-          <a>
-            <FaTiktok />
-          </a>
-          <a>
-            <FaInstagram />
-          </a>
+          <h1 className={styles.mapTitle}>Social media</h1>
+          <div className={styles.socialMediaContainer}>
+            <a>
+              <FaTiktok />
+            </a>
+            <a>
+              <FaInstagram />
+            </a>
+          </div>
+          <h1 className={styles.mapTitle}>Location</h1>
+          <p className={styles.locationText}>
+            Spot us across the road from Fellachidis Bakery
+          </p>
+          <Map />
         </div>
-        <h1 className={styles.mapTitle}>Location</h1>
-        <Map />
       </div>
     </div>
   );
