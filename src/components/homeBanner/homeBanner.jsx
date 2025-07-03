c8059bbc6c215324bd7a53a1c1a5356e72f3c6b3
Stavros <stavros.karachristianidis@alphaomegazed.com>
2025-07-03 21:32:17 +0300

    video fetched from youtube

--- src/components/homeBanner/homeBanner.jsx
+++ src/components/homeBanner/homeBanner.jsx
@@ -29,7 +29,7 @@ const HomeBanner = () => {
     };
   }, []);
   const videoUrl = isMobile
-    ? bgVideo
+    ? "https://youtube.com/shorts/9pCwIcMlnCw?feature=share"
     : "https://www.youtube-nocookie.com/embed/HSYBZ2MC9GU?autoplay=1&mute=1&loop=1&playlist=HSYBZ2MC9GU&controls=0&showinfo=0&modestbranding=1&rel=0&playsinline=1";
 
   return (
