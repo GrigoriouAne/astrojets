066d44a0cfc36905dc318ab9bf34b5eda3cbb40a
Stavros <stavros.karachristianidis@alphaomegazed.com>
2025-07-22 12:04:33 +0300

    remove 40 minutes plan

--- src/sections/ourPackagesSection/ourPackagesSection.jsx
+++ src/sections/ourPackagesSection/ourPackagesSection.jsx
@@ -8,8 +8,8 @@ const OurPackagesSection = () => {
       <div className={styles.cardsContainer}>
         <PricingCard minutes={15} image="basic-plan.png" price={40} />
         <PricingCard minutes={20} image="pro-plan.png" price={50} flame />
-        <PricingCard minutes={30} image="business-plan.png" price={60} />
-        <PricingCard minutes={40} image="enterprise-plan.png" price={80} />
+        <PricingCard minutes={30} image="business-plan.png" price={70} />
+        {/* <PricingCard minutes={40} image="enterprise-plan.png" price={80} /> */}
       </div>
     </div>
   );
